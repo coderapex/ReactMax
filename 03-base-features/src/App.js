555e9a6bd25160893a423535f75e0a22a3dbc105
coderapex <coder.apex@gmail.com>
2019-08-24 20:49:09 +0400

    Using 'state' to save values and use them to render

--- 03-base-features/src/App.js
+++ 03-base-features/src/App.js
@@ -3,14 +3,32 @@ import "./App.css";
 import Person from "./Person/person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "CoderApex", age: 30 },
+      { name: "Fozzy", age: 1 },
+      { name: "Stephan", age: 25 }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hello React!</h1>
-        <Person name="CoderApex" age="30" />
-        <Person name="Fozzy" age="1">
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
           I'm just a baby!
         </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }

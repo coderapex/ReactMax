b2b75835b575a7f1cdf8c855748f35ed506da869
coderapex <coder.apex@gmail.com>
2019-08-24 22:34:05 +0400

    Using 'setState()' to update state values

--- 03-base-features/src/App.js
+++ 03-base-features/src/App.js
@@ -8,13 +8,47 @@ class App extends Component {
       { name: "CoderApex", age: 30 },
       { name: "Fozzy", age: 1 },
       { name: "Stephan", age: 25 }
-    ]
+    ],
+    clickCount: 0
+  };
+
+  switchNameHandler = evt => {
+    // console.log("Click Event Triggered - Change Name Button");
+    // console.log(evt);
+
+    let newCount = this.state.clickCount + 1;
+
+    this.setState({
+      clickCount: newCount
+    });
+
+    if (newCount % 2 == 0) {
+      this.setState({
+        persons: [
+          { name: "CoderApex", age: 29 },
+          { name: "Fozzy", age: 1 },
+          { name: "Stephan", age: 25 }
+        ]
+      });
+    } else {
+      this.setState({
+        persons: [
+          { name: "Aamir Abbas", age: 30 },
+          { name: "Fozzy", age: 1 },
+          { name: "Stephan", age: 25 }
+        ]
+      });
+    }
   };
 
   render() {
     return (
       <div className="App">
         <h1>Hello React!</h1>
+
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <h6>Click Count : {this.state.clickCount}</h6>
+
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
